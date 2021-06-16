105cbc43ad6a3d1a108392abe460abc92ee5267a
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2021-06-16 14:43:21 -0600

    Add setup and requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name="delightful_discord_dlogger",
+        version="0.1.0",
+        packages=["delightful_discord_dlogger"],
+        entry_points={
+                'console_scripts': ['delightful_discord_dlogger.cli:main'],
+                },
+        description="Simple logging with Discord webhooks",
+        install_requires=['fire', 'requests'],
+        setup_requires=['fire', 'requests']
+        )
